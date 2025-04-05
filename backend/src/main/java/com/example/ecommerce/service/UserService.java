15bb11260d9bc8c44176a934fc05ca80a3dc3763
indupitta2003 <166369142+indu20711@users.noreply.github.com>
2025-04-05 12:59:17 +0530

    Create UserService.java

---
+++ backend/src/main/java/com/example/ecommerce/service/UserService.java
@@ -0,0 +1,24 @@
+package com.example.ecommerce.service;
+
+import com.example.ecommerce.model.User;
+import com.example.ecommerce.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepository;
+
+    public User registerUser(User user) {
+        return userRepository.save(user);
+    }
+
+    public User loginUser(String email, String password) {
+        User user = userRepository.findByEmail(email);
+        if (user != null && user.getPassword().equals(password)) {
+            return user;
+        }
+        return null;
+orki}
+}

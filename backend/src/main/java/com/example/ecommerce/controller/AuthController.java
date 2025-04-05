c4654a87e83768ba61dd1021171667ae1479d384
indupitta2003 <166369142+indu20711@users.noreply.github.com>
2025-04-05 12:59:50 +0530

    Create AuthController.java

---
+++ backend/src/main/java/com/example/ecommerce/controller/AuthController.java
@@ -0,0 +1,24 @@
+package com.example.ecommerce.controller;
+
+import com.example.ecommerce.model.User;
+import com.example.ecommerce.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@CrossOrigin(origins = "*")
+public class AuthController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping("/signup")
+    public User signup(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
+
+    @PostMapping("/login")
+    public User login(@RequestBody User user) {
+        return userService.loginUser(user.getEmail(), user.getPassword());
+    }
+}

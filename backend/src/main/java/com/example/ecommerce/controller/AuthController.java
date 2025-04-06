91a2097233600bdf9309ee991ae0ed386040b292
indupitta2003 <166369142+indu20711@users.noreply.github.com>
2025-04-06 12:56:28 +0530

    Update AuthController.java

--- backend/src/main/java/com/example/ecommerce/controller/AuthController.java
+++ backend/src/main/java/com/example/ecommerce/controller/AuthController.java
@@ -2,23 +2,28 @@ package com.example.ecommerce.controller;
 
 import com.example.ecommerce.model.User;
 import com.example.ecommerce.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
-@CrossOrigin(origins = "*")
 public class AuthController {
+    private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
+
     @Autowired
     private UserService userService;
 
     @PostMapping("/signup")
     public User signup(@RequestBody User user) {
+        logger.info("Signup request for email: {}", user.getEmail());
         return userService.registerUser(user);
     }
 
     @PostMapping("/login")
     public User login(@RequestBody User user) {
+        logger.info("Login request for email: {}", user.getEmail());
         return userService.loginUser(user.getEmail(), user.getPassword());
     }
 }
